f2569da17809b4542daf81c810befe60ccae9387
skyblue4cn <907572457@qq.com>
2014-01-20 18:13:22 +0800

    fix di                   郭建林               2014年1月20日15:21:54

--- src/main/java/com/bluesky/jeecg/framework/web/interceptor/DiHandlerInterceptor.java
+++ src/main/java/com/bluesky/jeecg/framework/web/interceptor/DiHandlerInterceptor.java
@@ -1,10 +1,12 @@
 package com.bluesky.jeecg.framework.web.interceptor;
 
 import com.bluesky.jeecg.framework.web.servlet.HandlerExecutionChain;
+import com.bluesky.jeecg.framework.web.tools.Classes;
 import org.springframework.web.method.HandlerMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +17,7 @@ import java.util.Map;
  */
 public class DiHandlerInterceptor implements IHandlerInterceptor {
         private Object[] parameterObject;
+    private String[] parameterNames;
     /**
      * 执行依赖转换
      * @param request
@@ -28,7 +31,8 @@ public class DiHandlerInterceptor implements IHandlerInterceptor {
         HandlerMethod handlerMethod=((HandlerExecutionChain) handler).getHandlerMethod();
 
         //获取参数
-        getAllParameters(handlerMethod);
+        getAllParameters(handlerMethod, handler);
+        //给参数设置值
         setValueOfParameters(request, response);
         return true;
     }
@@ -39,12 +43,13 @@ public class DiHandlerInterceptor implements IHandlerInterceptor {
      * 2014年1月20日10:58:14
      * @param handlerMethod  方法对象
      */
-    private Object[] getAllParameters(HandlerMethod handlerMethod)
+    private Object[] getAllParameters(HandlerMethod handlerMethod,Object handler)
     {
         //判断是否有值
         if( handlerMethod!=null)
         {
         parameterObject=   handlerMethod.getMethod().getParameterTypes();
+            parameterNames= Classes.getMethodParamNames(handler.getClass(),handlerMethod.getMethod().getName()+"");
         }
        return parameterObject;
     }
@@ -61,33 +66,34 @@ public class DiHandlerInterceptor implements IHandlerInterceptor {
          Map map=  request.getParameterMap();
          //封装后的对象列表
          Map<String,Object> ParametersValue=new HashMap<String, Object>();
+
+         int i=0;
          //循环参数列表
          for(Object target:parameterObject)
           {
-                 target =target.getClass().newInstance();
-              System.out.println(target.getClass());
+              // 调用getter方法获取属性值
+              Method m = (Method) target.getClass().getMethod(
+                      "getName");
+                        target.getClass().getName();
+              String val = (String) m.invoke(target);
               //判断是否是req和resp如果是则进行下次循环
-              if (target.getClass().getPackage().equals(request) )
+              if (val.equals("javax.servlet.http.HttpServletRequest") )
               {
                   target=request;
-                  ParametersValue.put(target.getClass().getName(),target);
+                  ParametersValue.put(parameterNames[i],target);
                   continue;
               }
-              if ( target.getClass().equals(response))
+              if ( val.equals("javax.servlet.http.HttpServletResponse"))
               {
                   target=response;
-                  ParametersValue.put(target.getClass().getName(),target);
+                  ParametersValue.put(parameterNames[i],target);
                   continue;
               }
               //如果是其他对象
-
-
-             if(target.getClass().getDeclaredFields().length>0)
-             {
-            target.getClass().getDeclaredFields()[0].getName();
-             }
-             System.out.println( target.getClass().getDeclaredFields()[0].getName());
+              ParametersValue.put(parameterNames[i], setValueOfSingeObject(map, target));
+              i++;
            }
+         System.out.println();
      }
     public Object[] getParameterObject() {
         return parameterObject;
@@ -96,4 +102,38 @@ public class DiHandlerInterceptor implements IHandlerInterceptor {
     public void setParameterObject(Object[] parameterObject) {
         this.parameterObject = parameterObject;
     }
+
+    /**
+     * 给具体一个对象赋值
+     * 郭建林
+     * 2014年1月20日16:13:17
+     * @param keyValue 键值对列表
+     * @param target  目标对象
+     */
+    public Object setValueOfSingeObject(Map<String,Object> keyValue,Object target)  throws Exception
+    {
+
+        for(String key : keyValue.keySet())
+        {
+        if(!target.getClass().isPrimitive())
+        {
+        // 调用getter方法获取属性值
+        Method m = (Method) target.getClass().getMethod(
+                "getName");
+        target.getClass().getName();
+        String val = (String) m.invoke(target);
+        target=Class.forName(val).newInstance();
+        if(target.getClass().getDeclaredFields().length>0)
+        {
+            target.getClass().getDeclaredFields()[0].getName();
+        }
+        }
+        else
+        {
+            target=keyValue.get(key);
+        }
+        }
+         return target;
+    }
+
 }
--- src/main/java/com/bluesky/jeecg/framework/web/servlet/DispatcherServlet.java
+++ src/main/java/com/bluesky/jeecg/framework/web/servlet/DispatcherServlet.java
@@ -4,13 +4,11 @@ import com.bluesky.jeecg.framework.web.httphandler.HandlerMapping;
 import com.bluesky.jeecg.framework.web.httphandler.InitMethodMapping;
 import org.springframework.context.ApplicationContext;
 import org.springframework.util.ClassUtils;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.util.UrlPathHelper;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.lang.reflect.Method;
 
 /**
  * User: bluesky
